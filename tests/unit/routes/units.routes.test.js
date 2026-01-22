904c74bddaccd148844cc51c16795e6f6c037db6
Tzvi <hersheyb@measuredleap.com>
2026-01-23 01:17:21 +0400

    typo fix

--- tests/unit/routes/units.routes.test.js
+++ tests/unit/routes/units.routes.test.js
@@ -1,5 +1,4 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import unitRoutes from '../../../src/routes/units.routes.js'; 
 import * as unitsController from '../../../src/controllers/units.controller.js';
 
 describe('Unit Routes', () => {
